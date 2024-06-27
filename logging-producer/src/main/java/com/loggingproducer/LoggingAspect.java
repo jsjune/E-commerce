064365c97272a9efed6aea093debbce1d4454f53
jsjune <avraskio@gmail.com>
2024-06-27 12:17:39 +0900

    logging-producer zipkin 설정 #15

--- logging-producer/src/main/java/com/loggingproducer/LoggingAspect.java
+++ logging-producer/src/main/java/com/loggingproducer/LoggingAspect.java
@@ -1,7 +1,9 @@
 package com.loggingproducer;
 
+import io.micrometer.tracing.Tracer;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
@@ -17,26 +19,32 @@ import org.springframework.stereotype.Component;
 public class LoggingAspect {
 
     private final LoggingProducer loggingProducer;
+    private final Tracer tracer;
 
     @Before("execution(* com.*.controller..*(..))")
     public void beforeMethodExecution(JoinPoint joinPoint) {
         String className = joinPoint.getTarget().getClass().getName();
         String methodName = joinPoint.getSignature().getName();
+        String traceId = Objects.requireNonNull(tracer.currentTraceContext().context()).traceId();
+        String spanId = Objects.requireNonNull(tracer.currentTraceContext().context()).spanId();
         log.info("Before executing method: " + className + "." + methodName);
         loggingProducer.sendMessage("logging-topic",
-            "Before executing method: " + className + "." + methodName);
+            "[" + traceId + "-" + spanId + "] Before executing method: " + className + "."
+                + methodName);
     }
 
     @AfterThrowing(pointcut = "execution(* com.*.controller..*(..))", throwing = "exception")
     public void afterThrowingMethodExecution(JoinPoint joinPoint, Throwable exception) {
         String className = joinPoint.getTarget().getClass().getName();
         String methodName = joinPoint.getSignature().getName();
+        String traceId = Objects.requireNonNull(tracer.currentTraceContext().context()).traceId();
+        String spanId = Objects.requireNonNull(tracer.currentTraceContext().context()).spanId();
         StringWriter sw = new StringWriter();
         PrintWriter pw = new PrintWriter(sw);
         exception.printStackTrace(pw);
         String stackTrace = sw.toString();
         loggingProducer.sendMessage("logging-topic",
-            "Exception thrown in method: " + className + "." + methodName + " Exception: "
+            "[" + traceId + "-" + spanId + "] Exception thrown in method: " + className + "." + methodName + " Exception: "
                 + exception.getMessage() + "\n Stack Trace: " + stackTrace);
     }
 
